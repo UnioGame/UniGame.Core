e2f6fb5e81ce3cfd379f3c15a686ac36f16dd809
Mefodei <mefodei.link@gmail.com>
2020-10-27 00:08:15 +0300

    - add rollback command

---
+++ Runtime/Interfaces/IAsyncRollback.cs
@@ -0,0 +1,12 @@
+﻿namespace UniModules.UniGame.Core.Runtime.Interfaces {
+    using Cysharp.Threading.Tasks;
+
+    public interface IAsyncRollback {
+        UniTask Rollback();
+    }
+    
+    public interface IAsyncRollback<TResult>
+    {
+        UniTask<TResult> Rollback();
+    }
+}
---
+++ Runtime/Interfaces/IAsyncRollbackCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace UniModules.UniGame.Core.Runtime.Interfaces
+{
+    public interface IAsyncRollbackCommand : IAsyncCommand, IAsyncRollback
+    {
+       
+    }
+    
+    public interface IAsyncRollbackCommand<T> : IAsyncCommand<T>,IAsyncRollback<T>
+    {
+        
+    }
+    
+    public interface IAsyncRollbackCommand<TCommand,TRollback> : IAsyncCommand<TCommand>,IAsyncRollback<TRollback>
+    {
+        
+    }
+}
