3896f86f2dece4efc7432d07050a5835e38d4454
Mefodei <mefodei.link@gmail.com>
2020-11-11 22:27:42 +0300

    update core package

---
+++ Runtime/Utils/TimeUtils.cs
@@ -0,0 +1,14 @@
+﻿    using System;
+
+    namespace UniModules.UniGame.Core.Runtime.Utils
+    {
+        public static class TimeUtils
+        {
+            private static readonly DateTime StartTime = new DateTime(1970,1,1,0,0,0,DateTimeKind.Utc);
+
+            public static long GetUtcNow()
+            {
+                return (long)(DateTime.UtcNow - StartTime).TotalMilliseconds;
+            }
+        }
+    }
