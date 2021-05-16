5663f7ac573988edcd11d8adf8d37a70fd11cf0e
Mefodei <mefodei.link@gmail.com>
2021-05-16 19:07:37 +0300

    - fix ccompiled activator

--- Runtime/Utils/CompiledActivator.cs
+++ Runtime/Utils/CompiledActivator.cs
@@ -1,25 +1,47 @@
-﻿
-
-namespace UniModules.UniCore.Runtime.Utils
+﻿namespace UniModules.UniCore.Runtime.Utils
 {
+    using System.Linq;
+    using Cysharp.Threading.Tasks;
     using System;
     using System.Linq.Expressions;
     using System.Reflection;
     
     public static class CompiledActivator
     {
-
-        private static readonly MemorizeItem<Type, ConstructorInfo> getDefaultConstructor = MemorizeTool.Memorize<Type, ConstructorInfo>(
+        public static readonly MemorizeItem<Type, Type[]> _constructorTypes =
+            MemorizeTool.Memorize<Type, Type[]>(
+                x =>
+                {
+                    var defaultConstructor = x.GetConstructor(Type.EmptyTypes);
+                    if (defaultConstructor!=null)
+                        return Type.EmptyTypes;
+                    
+                    var constructor = x
+                        .GetConstructors(BindingFlags.Public)
+                        .FirstOrDefault();
+                    
+                    if(constructor == null)
+                        return Type.EmptyTypes;
+                    
+                    var parameters = constructor.GetParameters();
+                    var types = parameters
+                        .Select(info => info.ParameterType)
+                        .ToArray();
+                    return types;
+                });
+        
+        private static readonly MemorizeItem<Type, ConstructorInfo> _getDefaultConstructor = MemorizeTool.Memorize<Type, ConstructorInfo>(
             x =>
             {
-                var ctor = x.GetConstructor(Type.EmptyTypes);
+                var parameters = _constructorTypes[x];
+                var ctor = x.GetConstructor(parameters);
                 return ctor;
             });
 
-        private static readonly MemorizeItem<Type, Delegate> lambdaActivator = MemorizeTool.Memorize<Type, Delegate>(type =>
+        private static readonly MemorizeItem<Type, Delegate> _lambdaDefaultActivator = MemorizeTool.Memorize<Type, Delegate>(type =>
         {
             //get default constructor
-            var ctor = getDefaultConstructor[type];
+            var ctor = _getDefaultConstructor[type];
             if (ctor == null) 
                 return null;
             
@@ -34,26 +56,74 @@ namespace UniModules.UniCore.Runtime.Utils
             return delegateObject;
         });
 
+       
         #region extensions
-        
+
+        /// <summary>
+        /// get parameters of type. If have default constructor then return Type.EmptyTypes or parameters of first public constructor
+        /// </summary>
+        /// <param name="type">source type</param>
+        /// <returns>array of constructor parameters</returns>
+        public static Type[] GetParameters(this Type type) => _constructorTypes[type];
                 
         public static bool HasDefaultConstructor(this Type type)
         {
-            var cnstr = getDefaultConstructor[type];
+            var cnstr = _getDefaultConstructor[type];
             return cnstr != null;
         }
 
-        public static object CreateWithDefaultConstructor(this Type type) => CreateInstance(type);
+        public static object CreateWithDefaultConstructor(this Type type)
+        {
+            return type.HasDefaultConstructor() == false ? null : CreateInstance(type);
+        }
 
         public static TType CreateWithDefaultConstructor<TType>(this Type type)
-            where TType : class => CreateInstance<TType>(type);
+            where TType : class => CreateWithDefaultConstructor(type) as TType;
+        
+        
+        public static object CreateObject(this Type type)
+        {
+            if (type.HasDefaultConstructor())
+                return type.CreateWithDefaultConstructor();
+            return null;
+        }
 
         #endregion
 
-        public static object CreateInstance(Type type) => lambdaActivator[type]?.DynamicInvoke();
-
-        public static TType CreateInstance<TType>(Type type) where TType : class => CreateInstance(type) as TType;
+        public static object CreateInstance(Type type, params object[] args) =>
+            _lambdaDefaultActivator[type]?.DynamicInvoke(args);
 
 
     }
+
+    public static class TypeValueActivator
+    {
+
+        public static async UniTask<TValue> CreateObjectAsync<TValue>(this ITypeValueProvider valueProvider) where TValue : class
+        {
+            var value = await valueProvider.CreateObjectAsync(typeof(TValue));
+            return value as TValue;
+        }
+
+        public static async UniTask<object> CreateObjectAsync(this ITypeValueProvider valueProvider,Type type)
+        {
+            var parameters = type.GetParameters();
+            foreach (var parameter in parameters)
+            {
+                var value = await valueProvider.ReceiveValueAsync(parameter);
+            }
+            
+            return null;
+        }
+
+        
+    }
+    
+    public interface ITypeValueProvider
+    {
+        public object GetValue(Type type);
+
+        public UniTask<object> ReceiveValueAsync(Type type);
+    }
+    
 }
