c2bfea1c68338f2c0a9c55a544f90192c11afb88
Mefodei <mefodei.link@gmail.com>
2021-09-19 04:41:04 +0300

    - update package version

--- Editor/EditorProcessors/ScriptableProcessorsControllerAsset.cs
+++ Editor/EditorProcessors/ScriptableProcessorsControllerAsset.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
-using Unity.EditorCoroutines.Editor;
-
-namespace UniModules.UniGame.Core.Editor.EditorProcessors
+﻿namespace UniModules.UniGame.Core.Editor.EditorProcessors
 {
+    using System.Collections;
+    using Unity.EditorCoroutines.Editor;
     using System.Collections.Generic;
     using System.Linq;
     using UniModules.Editor;
@@ -11,24 +10,21 @@ namespace UniModules.UniGame.Core.Editor.EditorProcessors
     using UnityEngine;
 
     [GeneratedAssetInfo("ScriptableProcessors")]
-    public class ScriptableProcessorsControllerAsset :
-        GeneratedAsset<ScriptableProcessorsControllerAsset>
+    public class ScriptableProcessorsControllerAsset : GeneratedAsset<ScriptableProcessorsControllerAsset>
     {
         #region static methods
 
         [InitializeOnLoadMethod]
-        [MenuItem(itemName:"UniGame/ScriptableProcessors/Restart")]
-        public static void Initialize()
-        {
-            EditorCoroutineUtility.StartCoroutineOwnerless(InitializeRoutine());
-        }
+        [MenuItem(itemName: "UniGame/ScriptableProcessors/Restart")]
+        public static void ProcessorsInitialize() => EditorApplication.delayCall +=
+            () => EditorCoroutineUtility.StartCoroutineOwnerless(InitializeRoutine());
 
         #endregion
 
         #region inspector
 
 #if ODIN_INSPECTOR
-        [Sirenix.OdinInspector.InlineEditor()]
+        [Sirenix.OdinInspector.InlineEditor]
 #endif
         public List<ScriptableObject> processors = new List<ScriptableObject>();
 
@@ -39,28 +35,17 @@ namespace UniModules.UniGame.Core.Editor.EditorProcessors
 #if ODIN_INSPECTOR
         [Sirenix.OdinInspector.Button]
 #endif
-        public void Restart()
-        {
-            Initialize();
-        }
+        public void Restart() => Initialize();
         
 #if ODIN_INSPECTOR
         [Sirenix.OdinInspector.Button]
 #endif
-        public void Start()
-        {
-            processors.OfType<IEditorProcess>().
-                ForEach(x => x.Start());
-        }
+        public void Start() =>  processors.OfType<IEditorProcess>().ForEach(x => x.Start());
 
 #if ODIN_INSPECTOR
         [Sirenix.OdinInspector.Button]
 #endif
-        public void Stop()
-        {
-            processors.OfType<IEditorProcess>().
-                ForEach(x => x.Stop());
-        }
+        public void Stop() => processors.OfType<IEditorProcess>().ForEach(x => x.Stop());
 
         private static IEnumerator InitializeRoutine()
         {
@@ -74,10 +59,11 @@ namespace UniModules.UniGame.Core.Editor.EditorProcessors
         {
             var asset = Asset;
             
-            var allProcessors = AssetEditorTools.
-                GetAssets<ScriptableObject>().
-                Where(x => x is IEditorProcess).
-                ToList();
+            var allProcessors = AssetEditorTools
+                .GetAssets<ScriptableObject>()
+                .Where(x => x)
+                .Where(x => x is IEditorProcess)
+                .ToList();
             
             asset.processors.Clear();
             asset.processors.AddRange(allProcessors);
