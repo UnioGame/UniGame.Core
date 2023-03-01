afa518d29e0fd8944515a19d110f32b544f21ade
Mefodei <mefodei.link@gmail.com>
2023-03-02 02:30:06 +0700

    - update extension methods - add single value component

---
+++ Runtime/Common/ISingleValueProperty.cs
@@ -0,0 +1,13 @@
+﻿namespace UniGame.Runtime.Common
+{
+    public interface ISingleValueProperty<TValue>
+    {
+        bool Has { get; }
+
+        void SetValue(TValue value);
+        
+        TValue Take();
+
+        TValue Look();
+    }
+}
