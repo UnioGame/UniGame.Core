5545880b4ece8e73e25dfaba7d15024d97df0b24
Doxako71 <pr.ulyanov@gmail,com>
2023-05-23 20:54:28 +0300

    Add mathf extension

---
+++ Runtime/Extension/MathfExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace UniGame.Core.Runtime.Extension
+{
+	using System.Runtime.CompilerServices;
+	using UnityEngine;
+
+	public static class MathfExtension
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float Abs(this float value)
+		{
+			return Mathf.Abs(value);
+		}
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int Abs(this int value)
+		{
+			return Mathf.Abs(value);
+		}
+	}
+}
