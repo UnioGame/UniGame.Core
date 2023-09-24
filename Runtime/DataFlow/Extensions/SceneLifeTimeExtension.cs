2d1a5ae246d4fcdc8e548df7b2aa34411298e0df
Mefodei <mefodei.link@gmail.com>
2023-09-24 15:31:04 +0700

    - update memorize tool

--- Runtime/DataFlow/Extensions/SceneLifeTimeExtension.cs
+++ Runtime/DataFlow/Extensions/SceneLifeTimeExtension.cs
@@ -1,42 +1,67 @@
-using System;
-using System.Collections.Generic;
-using UniModules.UniCore.Runtime.DataFlow;
-using UniGame.Core.Runtime;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
 namespace UniModules.UniGame.Core.Runtime.DataFlow.Extensions
 {
+    using System;
+    using System.Runtime.CompilerServices;
+    using global::UniGame.Core.Runtime;
+    using UniModules.UniCore.Runtime.DataFlow;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using UniCore.Runtime.Utils;
+
     public static class SceneLifeTimeExtension
     {
-        private static Dictionary<int,Scene> _activeScenes = new Dictionary<int,Scene>();
-        private static Dictionary<int,ILifeTime> _sceneLifeTimes = new Dictionary<int,ILifeTime>(8);
-
-        static SceneLifeTimeExtension()
-        {
-            SceneManager.sceneLoaded += (scene, mode) => _activeScenes[scene.handle] = scene;
-            SceneManager.sceneUnloaded += scene => _activeScenes.Remove(scene.handle);
-
-#if UNITY_EDITOR
-            UnityEditor.AssemblyReloadEvents.beforeAssemblyReload += () =>
+        private static MemorizeItem<string,ILifeTime> _sceneLifeTimes = MemorizeTool
+            .Memorize<string,ILifeTime>(sceneId =>
             {
-                _activeScenes.Clear();
-                _sceneLifeTimes.Clear();
-            };
-#endif
+                var sceneCount = SceneManager.sceneCount;
+                Scene scene = default;
 
-        }
+                for (int i = 0; i < sceneCount; i++)
+                {
+                    var targetScene = SceneManager.GetSceneAt(i);
+                    if(!sceneId.Equals(targetScene.path,StringComparison.OrdinalIgnoreCase))
+                        continue;
+                    scene = targetScene;
+                }
+                
+                if(string.IsNullOrEmpty(scene.path)) 
+                    return LifeTime.TerminatedLifetime;
+                
+                var sceneObject = new GameObject();
+                SceneManager.MoveGameObjectToScene(sceneObject,scene);
+                var sceneLifeTime = sceneObject.AddComponent<LifeTimeComponent>();
+                return sceneLifeTime;
+            });
 
-        public static ILifeTime GetSceneLifeTime(this Scene scene)
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        public static void Initialize()
         {
-            return _sceneLifeTimes.TryGetValue(scene.handle, out var lifeTime) 
-                ? lifeTime : LifeTime.TerminatedLifetime;
+            SceneManager.sceneUnloaded -= OnSceneUnloaded;
+            SceneManager.sceneUnloaded += OnSceneUnloaded;
+
+            static void OnSceneUnloaded(Scene scene)
+            {
+                _sceneLifeTimes.Remove(scene.path);
+            }
         }
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ILifeTime GetActiveSceneLifeTime()
+        {
+            var activeScene = SceneManager.GetActiveScene();
+            return GetSceneLifeTime(activeScene);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ILifeTime GetSceneLifeTime(this Scene scene)
+        {
+            return _sceneLifeTimes[scene.path];
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ILifeTime GetLifeTime(this Scene scene)
         {
-            return _sceneLifeTimes.TryGetValue(scene.handle, out var lifeTime) 
-                ? lifeTime : LifeTime.TerminatedLifetime;
+            return GetSceneLifeTime(scene);
         }
 
         public static ILifeTime GetLifeTime(this object asset)
@@ -50,7 +75,7 @@ namespace UniModules.UniGame.Core.Runtime.DataFlow.Extensions
             switch (asset)
             {
                 case Scene scene:
-                    return scene.GetLifeTime();
+                    return scene.GetSceneLifeTime();
                 case ILifeTime lifeTime:
                     return lifeTime;
                 case ILifeTimeContext lifeTimeContext:
@@ -60,35 +85,25 @@ namespace UniModules.UniGame.Core.Runtime.DataFlow.Extensions
             return LifeTime.TerminatedLifetime;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ILifeTime AddTo(this IDisposable disposable, Scene scene)
         {
-            return AddTo(disposable, scene.handle);
+            return AddTo(disposable, scene.path);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ILifeTime AddToActiveScene(this IDisposable disposable)
+        {
+            var activeScene = SceneManager.GetActiveScene();
+            return AddTo(disposable, activeScene.path);
         }
 
-        public static ILifeTime AddTo(this IDisposable disposable,int sceneId)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ILifeTime AddTo(this IDisposable disposable,string sceneId)
         {
-            if (!_activeScenes.ContainsKey(sceneId))
-            {
-                disposable.Dispose();
-                return LifeTime.TerminatedLifetime;
-            }
-
-            if (!_sceneLifeTimes.TryGetValue(sceneId, out var sceneLifeTime))
-            {
-                var scene = _activeScenes[sceneId];
-                sceneLifeTime = CreateSceneLifeTime(scene);
-                sceneLifeTime.AddCleanUpAction(() => _sceneLifeTimes.Remove(sceneId));
-            }
-
+            var sceneLifeTime = _sceneLifeTimes[sceneId];
             return sceneLifeTime.AddDispose(disposable);
         }
 
-        public static ILifeTime CreateSceneLifeTime(Scene scene)
-        {
-            var sceneObject = new GameObject();
-            SceneManager.MoveGameObjectToScene(sceneObject,scene);
-            var lifeTime = sceneObject.AddComponent<LifeTimeComponent>();
-            return lifeTime;
-        }
     }
 }
--- Runtime/Utils/MemorizeItem.cs
+++ Runtime/Utils/MemorizeItem.cs
@@ -2,14 +2,20 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
 
     public class MemorizeItem<TKey, TData> : IDisposable
     {
         private readonly Func<TKey, TData> factory;
         private readonly Action<TData>     disposableAction;
-        
         private Dictionary<TKey,TData> _cache = new Dictionary<TKey,TData>(16);
 
+        public MemorizeItem(Action<TData> disposableAction = null)
+        {
+            this.factory          = null;
+            this.disposableAction = disposableAction;
+        }
+        
         public MemorizeItem(Func<TKey, TData> factory, Action<TData> disposableAction = null)
         {
             this.factory          = factory;
@@ -21,19 +27,30 @@
             get =>  GetValue(x);
             set => _cache[x] = value;
         }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool ContainsKey(TKey key) => _cache.ContainsKey(key);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryGetValue(TKey key, out TData value) => _cache.TryGetValue(key, out value);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool Remove(TKey key) => _cache.Remove(key);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public TData GetValue(TKey key)
         {
             if (_cache.TryGetValue(key, out var value) && value != null) 
                 return value;
+
+            if (factory == null) return default;
             
             value       = factory(key);
             _cache[key] = value;
             return value;
         }
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Dispose()
         {
             if (disposableAction != null) {
