104fceca24107af026f2d3b99f4b0b53ab00a6e2
pr.ulyanov <pr.ulyanov@gmail.com>
2023-11-30 12:13:10 +0300

    Add new mathf extension

--- Runtime/Extension/MathfExtension.cs
+++ Runtime/Extension/MathfExtension.cs
@@ -16,5 +16,15 @@
 		{
 			return Mathf.Abs(value);
 		}
+		
+		public static float Clamp(this float value, float min, float max)
+		{
+			return Mathf.Clamp(value, min, max);
+		}
+		
+		public static int Clamp(this int value, int min, int max)
+		{
+			return Mathf.Clamp(value, min, max);
+		}
 	}
 }
