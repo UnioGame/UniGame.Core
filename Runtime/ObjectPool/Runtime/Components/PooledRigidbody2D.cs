d0c9aded418bab68450a95bdd0fe67b08cb40189
Mefodei <mefodei.link@gmail.com>
2024-08-09 14:26:50 +0700

    - fix for unity 6

--- Runtime/ObjectPool/Runtime/Components/PooledRigidbody2D.cs
+++ Runtime/ObjectPool/Runtime/Components/PooledRigidbody2D.cs
@@ -1,29 +1,29 @@
 ﻿namespace UniGame.Runtime.ObjectPool.Components
 {
-	using UnityEngine;
+    using UnityEngine;
 
-	// This component will automatically reset a Rigidbody2D when it gets spawned/despawned
-	[RequireComponent(typeof(Rigidbody2D))]
-	public class PooledRigidbody2D : MonoBehaviour
-	{
-		// This gets called as soon as the clone is spawned
-		protected virtual void OnSpawn()
-		{
-			// Do nothing
-		}
-		
-		// This gets called just before the clone is despawned
-		protected virtual void OnDespawn()
-		{
-			var rigidbody2D = GetComponent<Rigidbody2D>();
-			
-			// Reset velocities
+    // This component will automatically reset a Rigidbody2D when it gets spawned/despawned
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PooledRigidbody2D : MonoBehaviour
+    {
+        // This gets called as soon as the clone is spawned
+        protected virtual void OnSpawn()
+        {
+            // Do nothing
+        }
+
+        // This gets called just before the clone is despawned
+        protected virtual void OnDespawn()
+        {
+            var rigidbody2DComponent = GetComponent<Rigidbody2D>();
+
+            // Reset velocities
 #if UNITY_6000_0_OR_NEWER
-			rigidbody2D.velocity= Vector2.zero;
+            rigidbody2DComponent.linearVelocity = Vector2.zero;
 #else
-			rigidbody2D.linearVelocity        = Vector2.zero;
+			rigidbody2DComponent.velocity = Vector2.zero;
 #endif
-			rigidbody2D.angularVelocity = 0.0f;
-		}
-	}
+            rigidbody2DComponent.angularVelocity = 0.0f;
+        }
+    }
 }
--- Runtime/Physics/Rigidbody2DVelocityLimiter.cs
+++ Runtime/Physics/Rigidbody2DVelocityLimiter.cs
@@ -21,6 +21,16 @@
         public void FixedUpdate()
         {
 #if UNITY_6000_0_OR_NEWER
+            if (UseMaxValocity && Rigidbody2D.linearVelocity.sqrMagnitude > _sqrMaxValocity)
+            {
+                Rigidbody2D.linearVelocity = Rigidbody2D.linearVelocity.normalized * MaxVelocity;
+            }
+
+            if (UseMinValocity && Rigidbody2D.linearVelocity.sqrMagnitude < _sqrMinVelocity)
+            {
+                Rigidbody2D.linearVelocity = Rigidbody2D.linearVelocity.normalized * MinVelocity;
+            }
+#else
             if (UseMaxValocity && Rigidbody2D.velocity.sqrMagnitude > _sqrMaxValocity)
             {
                 Rigidbody2D.velocity = Rigidbody2D.velocity.normalized * MaxVelocity;
@@ -29,16 +39,6 @@
             if (UseMinValocity && Rigidbody2D.velocity.sqrMagnitude < _sqrMinVelocity)
             {
                 Rigidbody2D.velocity = Rigidbody2D.velocity.normalized * MinVelocity;
-            }
-#else
-			if (UseMaxValocity && Rigidbody2D.linearVelocity.sqrMagnitude > _sqrMaxValocity)
-            {
-                Rigidbody2D.linearVelocity = Rigidbody2D.linearVelocity.normalized * MaxVelocity;
-            }
-
-            if (UseMinValocity && Rigidbody2D.linearVelocity.sqrMagnitude < _sqrMinVelocity)
-            {
-                Rigidbody2D.linearVelocity = Rigidbody2D.linearVelocity.normalized * MinVelocity;
             }
 #endif
 
