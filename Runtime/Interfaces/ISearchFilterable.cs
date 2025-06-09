3e91abc2ba3a8399f216fb5b3b32f280f5385c83
Mefodei <mefodei.link@gmail.com>
2025-06-09 22:42:55 +0700

    - fix odin usages

---
+++ Runtime/Interfaces/ISearchFilterable.cs
@@ -0,0 +1,25 @@
+﻿namespace UniGame.Core.Runtime
+{
+#if !ODIN_INSPECTOR
+    public interface ISearchFilterable
+    {
+        bool IsMatch(string filter);
+    }
+    
+    public class ValueDropdownItem<TValue>
+    {
+        public string Text = string.Empty;
+        public TValue Value = default;
+        
+        public ValueDropdownItem(){}
+        
+        public ValueDropdownItem(string text, TValue value)
+        {
+            Text = text;
+            Value = value;
+        }
+    }
+    
+#endif
+
+}
