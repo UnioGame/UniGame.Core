58657422f4777875000a2a0f8e0662c2d36806e2
Mefodei <mefodei.link@gmail.com>
2025-06-09 18:42:06 +0700

    - update version 2025.0.1

---
+++ Runtime/DataFlow/GameService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UniGame.GameFlow.Runtime;
+
+namespace UniGame.UniNodes.GameFlow.Runtime
+{
+    using Cysharp.Threading.Tasks;
+    using UniGame.Runtime.DataFlow;
+    using Core.Runtime;
+
+    /// <summary>
+    /// base game service class for binding Context source data to service logic
+    /// </summary>
+    [Serializable]
+    public abstract class GameService : IGameService
+    {
+        private readonly LifeTimeDefinition _lifeTimeDefinition = new();
+
+        public void Dispose() => _lifeTimeDefinition.Terminate();
+
+        public ILifeTime LifeTime => _lifeTimeDefinition;
+
+        public virtual UniTask InitializeAsync() { return UniTask.CompletedTask; }
+    }
+}
---
+++ Runtime/DataFlow/IGameService.cs
@@ -0,0 +1,12 @@
+﻿using UniGame.Core.Runtime;
+
+namespace UniGame.GameFlow.Runtime
+{
+    using System;
+
+    public interface IGameService : 
+        IDisposable, 
+        ILifeTimeContext
+    {
+    }
+}
